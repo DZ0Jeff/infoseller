3a8e3af814bf9d997771bebce1807c517b9a89a1
Jeferson <mx,jeferson.12@gmail.com>
2022-05-03 17:59:56 -0300

    add error handler in details

--- infoseller/__main__.py
+++ infoseller/__main__.py
@@ -15,17 +15,40 @@ def find_tag(tag, html, error_msg='Não localizado...'):
 
 
 def crawler(request):
-    request.html.render(timeout=30, scrolldown=10, sleep=1)
-    container = request.html.find('div.JIIxO', first=True)
+    request.html.render(timeout=30, scrolldown=10, sleep=5)
+    html = request.html
+    container = html.find('div.JIIxO', first=True)
+
+    if len(container.find('a')) == 0:
+        return
 
     for product in container.find('a'):
         try:
             title = product.find('h1', first=True).text
             price = product.find('.mGXnE._37W_B', first=True).text
-            fullPrice = product.find('._11_8K span', first=True).text
-            discount = product.find('._2LVIV', first=True).text
-            sold = product.find('._1kNf9', first=True).text
-            rate = product.find('.eXPaM', first=True).text
+            try:
+                fullPrice = product.find('._11_8K span', first=True).text
+            
+            except AttributeError:
+                fullPrice = '-'
+
+            try:
+                discount = product.find('._2LVIV', first=True).text
+            
+            except AttributeError:
+                discount = '-'
+
+            try:
+                sold = product.find('._1kNf9', first=True).text
+            
+            except AttributeError:
+                sold = '-'
+
+            try:
+                rate = product.find('.eXPaM', first=True).text
+            
+            except AttributeError:
+                rate = '-'
 
             data = {
                 'Nome': title,
@@ -40,16 +63,27 @@ def crawler(request):
             [print(f"{key}: {content}") for key, content in data.items()]
             compressed_data = { key: [value] for key, value in data.items() if value != 'Não localizado...' }
             dataToCSV(compressed_data, 'aliexpress.csv')
+            return True
 
         except AttributeError:
             logging.error('Não foi possível obter os dados...')
+        
+    
     
 
 def main():
     session = HTMLSession()
-    request = session.get('https://pt.aliexpress.com/category/201000006/computer-office.html')
-    crawler(request)
+    page =  1
+    while True: 
+        request = session.get(f'https://pt.aliexpress.com/category/201000006/computer-office.html&page={page}')
+        if not crawler(request):
+            break
+        
+        else:
+            page += 1
 
 
 if __name__ == '__main__':
+    print('Starting infoseller...')
     main()
+    print('Done!')
